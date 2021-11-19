a21494e48e6b2a3f4b9dcf536143d8c587f04c95
Wahid Hidayat <wshidayat38@gmail.com>
2021-11-19 09:39:28 +0700

    first commit

---
+++ GithubUserApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GithubUserApp
+//
+//  Created by Wahid Hidayat on 19/11/21.
+//  Copyright © 2021 Wakhid Saiful Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'GithubUserApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GithubUserApp
+  pod 'SwiftLint'
+  pod 'Alamofire', '~> 5.4'
+  pod 'Kingfisher', '~> 6.0'
+end
